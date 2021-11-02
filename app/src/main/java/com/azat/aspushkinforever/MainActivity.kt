677cca43dfc8da80b5b221ec05adf8d2194834e3
Azat <azat-sr@mail.ru>
2021-11-02 18:22:04 +0300

    commit all files

--- app/src/main/java/com/azat/aspushkinforever/MainActivity.kt
+++ app/src/main/java/com/azat/aspushkinforever/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(My_Own_Log_TAG, "onCreate()")
+
         textView1 = findViewById(R.id.text_view1)
         textView2 = findViewById(R.id.text_view2)
         textView3 = findViewById(R.id.text_view3)
@@ -39,35 +39,53 @@ class MainActivity : AppCompatActivity() {
         textView10 = findViewById(R.id.text_view10)
         textView11 = findViewById(R.id.text_view11)
         textView12 = findViewById(R.id.text_view12)
+
+        //Log.d(My_Own_Log_TAG, "onCreate()")
+
     }
     override fun onStart() {
         super.onStart()
-        Log.d(My_Own_Log_TAG, "onStart()")
+        //Log.d(My_Own_Log_TAG, "onStart()")
+        Log.d(My_Own_Log_TAG, textView1.text.toString())
+        Log.d(My_Own_Log_TAG, textView2.text.toString())
+        Log.d(My_Own_Log_TAG, textView3.text.toString())
+
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d(My_Own_Log_TAG, "onResume()")
+        //Log.d(My_Own_Log_TAG, "onResume()")
+        Log.d(My_Own_Log_TAG, textView4.text.toString())
+        Log.d(My_Own_Log_TAG, textView5.text.toString())
+        Log.d(My_Own_Log_TAG, textView6.text.toString())
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d(My_Own_Log_TAG, "onPause()")
+        //Log.d(My_Own_Log_TAG, "onPause()")
+        Log.d(My_Own_Log_TAG, textView7.text.toString())
+        Log.d(My_Own_Log_TAG, textView8.text.toString())
+        Log.d(My_Own_Log_TAG, textView9.text.toString())
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d(My_Own_Log_TAG, "onStop()")
+        //Log.d(My_Own_Log_TAG, "onStop()")
+        Log.d(My_Own_Log_TAG, textView10.text.toString())
+        Log.d(My_Own_Log_TAG, textView11.text.toString())
+        Log.d(My_Own_Log_TAG, textView12.text.toString())
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.d(My_Own_Log_TAG, "onRestart()")
-    }
+        //Log.d(My_Own_Log_TAG, "onRestart()")
+           }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(My_Own_Log_TAG, "onDestroy()")
+        //Log.d(My_Own_Log_TAG, "onDestroy()")
+
+
     }
 
 }
